a3d6dbaece6ec0e98654d6b02814284fe650fa5f
Cliff Crerar <cliff.crerar@gmail.com>
2022-09-18 11:44:22 +0200

    fixing ci

--- next.config.js
+++ next.config.js
@@ -3,16 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: '/docs',
-
-  images: {
-    unoptimized: false
-  }
 }
 
-if (process.env.NODE_ENV !== 'development') {
-  nextConfig.images.unoptimized = false;
 
-}
 if (process.env.NODE_ENV === 'development')
   nextConfig.devIndicators = {
     autoPrerender: false,
