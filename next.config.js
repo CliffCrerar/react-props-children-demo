614ce052449a41460bb4b11c279d601c6f843373
Cliff Crerar <cliff.crerar@gmail.com>
2022-09-18 02:15:01 +0200

    mir

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const nextConfig = {
   }
 }
 
-if(process.env.NODE_ENV !== 'development') {
-  nextConfig.images.unoptimized = false;
-}
-nextConfig.images.unoptimized = false;
+// if(process.env.NODE_ENV !== 'development') {
+//   nextConfig.images.unoptimized = false;
+// }
+// nextConfig.images.unoptimized = false;
 module.exports = nextConfig
